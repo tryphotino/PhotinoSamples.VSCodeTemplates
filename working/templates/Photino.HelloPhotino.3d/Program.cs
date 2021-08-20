ada9da524054ff1da3cbadeb1ce29105ef6a1444
Mike Yeager <myeager-msid@outlook.com>
2021-08-20 12:52:29 -0600

    New templates added.

---
+++ working/templates/Photino.HelloPhotino.3d/Program.cs
@@ -0,0 +1,31 @@
+﻿using PhotinoNET;
+using System;
+using System.Drawing;
+
+namespace HelloPhotino.ThreeD
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            // Window title declared here for visibility
+            string windowTitle = "Photino 3D Pong";
+
+            // Creating a new PhotinoWindow instance with the fluent API
+            var window = new PhotinoWindow()
+                .SetTitle(windowTitle)
+                // Resize to a percentage of the main monitor work area
+                .SetUseOsDefaultSize(false)
+                .SetSize(new Size(700, 525))
+                // Center window in the middle of the screen
+                .Center()
+                // Users can resize windows by default.
+                // Let's make this one fixed instead.
+                .SetResizable(false)
+                .Load("wwwroot/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
+
+            window.WaitForClose(); // Starts the application event loop
+        }
+    }
+}
---
+++ working/templates/Photino.HelloPhotino.MultiWindow/Program.cs
@@ -0,0 +1,70 @@
+﻿using PhotinoNET;
+using System;
+
+namespace HelloWorldApp
+{
+    class Program
+    {
+        static int _childCount;
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new PhotinoWindow()
+                .SetTitle("Main Window")
+                .RegisterWebMessageReceivedHandler(CloseWindowMessageDelegate)
+                .RegisterWebMessageReceivedHandler(NewWindowMessageDelegate)
+                .SetUseOsDefaultSize(false)
+                .SetWidth(600)
+                .SetHeight(400)
+                .Center()
+                .Load("wwwroot/main.html")
+                .WaitForClose();
+        }
+
+        static void CloseWindowMessageDelegate(object sender, string message)
+        {
+            var window = (PhotinoWindow)sender;
+            
+            if (message == "close-window")
+            {
+                Console.WriteLine($"Closing \"{window.Title}\".");
+                window.Close();
+            }
+        }
+
+        static void NewWindowMessageDelegate(object sender, string message)
+        {
+            var window = (PhotinoWindow)sender;
+
+            if (message == "random-window")
+            {
+                var random = new Random();
+
+                int workAreaWidth = window.MainMonitor.WorkArea.Width;
+                int workAreaHeight = window.MainMonitor.WorkArea.Height;
+
+                int width = random.Next(400, 800);
+                int height = (int)Math.Round(width * 0.625, 0);
+
+                int offset = 20;
+                int left = random.Next(offset, workAreaWidth - width - offset);
+                int top = random.Next(offset, workAreaHeight - height - offset);
+
+                _childCount++;
+
+                new PhotinoWindow()
+                    .SetTitle($"Random Window ({_childCount})")
+                    .SetUseOsDefaultSize(false)
+                    .SetHeight(height)
+                    .SetWidth(width)
+                    .SetUseOsDefaultLocation(false)
+                    .SetTop(top)
+                    .SetLeft(left)
+                    .RegisterWebMessageReceivedHandler(CloseWindowMessageDelegate)
+                    .Load("wwwroot/random.html")
+                    .WaitForClose();
+            }
+        }
+    }
+}
